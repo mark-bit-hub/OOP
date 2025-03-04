36fc383da4945b978111a34aa6b298a25fbd5aaf
Oyoodaniel <155971702+mark-bit-hub@users.noreply.github.com>
2025-03-04 16:58:51 +0300

    Checks whether a given year is a leap year or not
    This program checks whether a given year is a leap year or not. It uses the modulus operator (%) to determine if the year is divisible by 4.

---
+++ IsLeapYear.java
@@ -0,0 +1,7 @@
+public class IsLeapYear {
+    public static void main(String[] args) {
+        int year = 2025;
+        if (year % 4 != 0) System.out.println("Non Leap year!");
+        else System.out.println("Leap year!");
+    }
+}
